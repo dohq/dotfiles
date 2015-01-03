c63f3a2befbf55020c7baf24ea52ca8e4da9dc2d
dohq <dorastone@gmail.com>
2015-01-04 05:29:13 +0900

    add .gitconfig

--- setup.sh
+++ setup.sh
@@ -3,19 +3,11 @@
 ENV="~/.zshenv"
 RC="~/.zshrc"
 VIMRC="~/.vimrc"
-
-if [ -f ${ENV} ]; then
-    /bin/mv ${ENV} ${ENV}_bak
-elif
-    [ -f ${RC} ]; then
-    /bin/mv ${RC} ${RC}_bak
-elif
-    [ -f ${VIMRC} ]; then
-    /bin/mv ${VIMRC} ${VIMRC}_bak
-fi
+GIT_CONFIG="~/.gitconfig"
 
 ln -s ~/dotfiles/.zshenv ${ENV}
 ln -s ~/dotfiles/.zshrc ${RC}
 ln -s ~/dotfiles/.vimrc ${VIMRC}
+ln -s ~/dotfiles/.gitconfig ${GIT_CONFIG}
 
 exit
