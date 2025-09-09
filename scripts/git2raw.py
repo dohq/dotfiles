390376f2f38d84bc4ae98d047f1498f48c9ca8a8
dohq <dorastone@gmail.com>
2025-09-10 08:13:21 +0900

    chore(scripts): remove unused scripts

--- scripts/git2raw.py
+++
@@ -1,9 +0,0 @@
-# your link goes here
-link = "https://github.com/Japont/Japont-fonts/blob/54e809086cfca96a9f7459f70dfe65476660f58d/fontna-mplus/NikumaruFont.woff"
-
-# note: this will break if a repo/organization or subfolder is named "blob" -- would be ideal to use a fancy regex
-# to be more precise here
-print(link.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/"))
-
-# example output link:
-# https://raw.githubusercontent.com/knightlab-analyses/qurro-mackerel-analysis/master/AnalysisOutput/qurro-plot.qzv
--- scripts/network_info.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-
-ipAddress=$(ip a | grep -e wlp1s0 -e enp3s0 | grep inet | awk '{print $2}')
-
-signalPower=$(cat /proc/net/wireless | tail -1 | tr -s ' ' | cut -d' ' -f4 | sed 's/\./dBm/')
-
-echo "${ipAddress}" "${signalPower}"
